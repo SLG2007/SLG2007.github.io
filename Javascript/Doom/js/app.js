7a7f42c62dbf8bdbbd581a8faea43c94444a5485
Sara Lopez <10892357@iesmarxadella.org>
2026-03-05 11:50:55 +0100

    acabado bucle

--- Javascript/Doom/js/app.js
+++ Javascript/Doom/js/app.js
@@ -1,14 +1,19 @@
 //variables
 let num1=document.getElementById("numero1")
+let num2=document.getElementById("numero2")
 let boton=document.getElementById("run")
 
 
 //eventos
 boton.addEventListener("click", (e) => {
-    console.log("Me han pulsado: " + num1.value*2)
+    /*console.log("El resultado de " + num1.value + " por " + num2.value + " es " + (parseInt(num1.value) + parseInt(num2.value)))
+
+    salida.innerHTML=("El resultado de " + num1.value + " por " + num2.value + " es " + (parseInt(num1.value) + parseInt(num2.value)))*/
+    salida.innerHTML=""
+    for (let i=0; i<parseInt(num2.value); i++) {
+        salida.innerHTML+=num1.value + "<br>"
+    }
+
 })
 
 //main
-
-console.log(num1.value)
-console.log(boton.id)
