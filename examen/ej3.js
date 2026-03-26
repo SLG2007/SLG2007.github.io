4ef08f32480abe68c3f4125ea179aaf68103e7b1
Sara Lopez <10892357@iesmarxadella.org>
2026-03-26 11:34:18 +0100

    examen corregido

--- examen/ej3.js
+++ examen/ej3.js
@@ -1,36 +1,39 @@
-//numero del cesar
+//frase boomer
 
+let boton = document.getElementById("boton")
+let texto = document.getElementById("texto")
 let resultado = document.getElementById("resultado")
-let frase = document.getElementById("frase")
 let fraseBoomer = ""
 let numMay = numeroAleatorio(1,2)
 
-function numeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function numeroAleatorio(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function cambiaVocales(frase) {
+    let nuevaFrase=""
+    for (let i=0; i<frase.length; i++) {
+        console.log(frase[i])
+        if (frase[i] == 'a' || frase[i] == 'A') nuevaFrase+="4"
+        else if (frase[i] == 'e' || frase[i] == 'E') nuevaFrase+="3"
+        else if (frase[i] == 'i' || frase[i] == 'I') nuevaFrase+="1"
+        else if (frase[i] == 'o' || frase[i] == 'O') nuevaFrase+="0"
+        else if (numeroAleatorio(2)==1) nuevaFrase+=frase[i]
+        else nuevaFrase+=frase[i].toUpperCase(); 
+    }
+    return nuevaFrase
 }
 
 boton.addEventListener("click", (e) => {
-    let opcion = numeroAleatorio(1,2)
     
-    for (let i=0; i<=frase.length; i++) {
-        let opcion = numeroAleatorio(1,2)
-        
-        if (opcion == 1) {
-            fraseBoomer+=frase[0]
-        }
-        else
-            pswd += caracteres[numeroAleatorio(0,caracteres.length-1)]
-
-    }
-        
-
-        
-    }
-
-
-    resultado.innerHTML+=fraseBoomer
+    //let texto=frase.value + "<br>"
     
-
+    let texto=cambiaVocales(frase.value)
+    resultado.innerHTML+=texto + "<br>"
+        
+        
+      
+        
 })
 
 
